72162168408f2d5747b6c83e20a0a38f5ce404ba
yakoSin <yakoSin@users.noreply.github.com>
2022-09-23 12:41:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
+corsOptions = { origin: true }
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!  V2 👋`);
+  res.send(`Hello ${name}! V3 👋`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
