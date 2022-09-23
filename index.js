cb4360ff5158e4ce2a6e07581df0ad06520243d8
yakoSin <yakoSin@users.noreply.github.com>
2022-09-23 12:24:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}! 👋`);
+  res.send(`Hello ${name}!  V2 👋`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
