44468ab02b779d33f37c05d6b3bc7b06a675df37
yakoSin <yakoSin@users.noreply.github.com>
2022-09-23 11:34:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,3 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
